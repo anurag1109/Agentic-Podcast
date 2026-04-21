a034732bfb577c52be56a1ca43b6143999507fc8
anurag1109 <bittu.anurag123@gmail.com>
2026-04-22 00:07:02 +0530

    generic port

--- src/server.js
+++ src/server.js
@@ -3,4 +3,6 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use("/api/podcast", require("./routes/podcast.routes"));
-app.listen(process.env.PORT || 5000, () => console.log("running"));
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server is running at PORT ${process.env.PORT || 5000}...`),
+);
